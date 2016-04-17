3bb34bae5f0a77dbba2ce077f86e57afce7948cb
Peter Yetti <pyetti@users.noreply.github.com>
2016-04-17 13:03:30 -0400

    refactored

--- src/main/java/com/excelparser/persister/SheetDaoImpl.java
+++ src/main/java/com/excelparser/persister/SheetDaoImpl.java
@@ -14,24 +14,31 @@ import com.excelparser.spreadsheet.SpreadSheet;
 
 public class SheetDaoImpl<K, V> implements SheetDao<K, V> {
 
-	private final static Logger logger = Logger.getLogger(SheetDaoImpl.class);
+	private final static Logger logger = Logger.getLogger(SheetDaoImpl.class.getName());
 	private DataSource dataSource;
-	private String createSql;
+	private String insertQuery;
 
-	public SheetDaoImpl(final DataSource dataSource, final String createSql) {
+	public SheetDaoImpl(final DataSource dataSource, final String insertQuery) {
 		this.dataSource = dataSource;
-		this.createSql = createSql;
+		this.insertQuery = insertQuery;
+	}
+
+	public SheetDaoImpl(final Database<K, V> database) {
+		this.dataSource = database.getDataSource();
+		this.insertQuery = database.getSql();
 	}
 
 	@Override
 	public int create(final SpreadSheet<K, V> spreadSheet) {
+		logger.info("Persisting " + spreadSheet.getRowCount() + " rows.");
 		Connection conn = null;
 		PreparedStatement pst = null;
 		try {
 			conn = dataSource.getConnection();
 			if (conn != null) {
-				int parameters = StringUtils.countMatches(createSql, "?");
-				pst = conn.prepareStatement(createSql);
+				int parameters = StringUtils.countMatches(insertQuery, "?");
+				pst = conn.prepareStatement(insertQuery);
+				int numRowsUploaded = 0;
 				for(int rowNum = spreadSheet.getStartRowNum(); rowNum <= spreadSheet.getEndRowNum(); rowNum++) {
 					List<V> row = spreadSheet.getRow(rowNum);
 					if (row != null && row.size() == parameters) {
@@ -42,27 +49,36 @@ public class SheetDaoImpl<K, V> implements SheetDao<K, V> {
 					}
 				}
 				spreadSheet.clear();
-				return pst.executeBatch().length;
+				numRowsUploaded += pst.executeBatch().length;
+				pst.clearBatch();
+				logger.info("Number of rows persisted: " + numRowsUploaded);
+				return numRowsUploaded;
 			}
+			logger.error("Failed to connect to database.");
 		} catch (SQLException e) {
 			logger.error(e.getMessage(), e);
+			return -1;
 		} finally {
-			if (pst != null) {
-				try {
-					pst.close();
-				} catch (SQLException e) {
-					logger.error(e.getMessage(), e);
-				}
-			}
-			if (conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException e) {
-					logger.error(e.getMessage(), e);
-				}
-			}
+			close(conn, pst);
 		}
 		return 0;
+	}
+
+	private void close(Connection conn, PreparedStatement pst) {
+		if (pst != null) {
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				logger.error(e.getMessage(), e);
+			}
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				logger.error(e.getMessage(), e);
+			}
+		}
 	}
 
 }
